c330d57e17c13cd049a72c9b6768c005d40547bb
manaals <manaals@cep.org>
2025-10-01 14:35:56 -0400

    remove tailwind and add basic css styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import { AuthProvider } from '@/app/hooks/useAuth';
 import './globals.css';
+import './elections.css';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
